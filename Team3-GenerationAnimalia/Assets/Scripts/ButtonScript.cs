c3212e84fe65683c651705e3318e15c5b4838df1
Cameron <cameronjamesbrown21@gmail.com>
2022-03-04 11:02:33 +0000

    ButtonScript Start
    CB: ButtonScript start made

--- Team3-GenerationAnimalia/Assets/Scripts/ButtonScript.cs
+++ Team3-GenerationAnimalia/Assets/Scripts/ButtonScript.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class ButtonScript : MonoBehaviour
 {
+    public bool buttonOn;
+    public TileBase onTile, offTile;
+
+    private Tilemap tilemap;
+    private TileBase[] allTile;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        buttonOn = false;
+        tilemap = GetComponent<Tilemap>();
+        allTile = tilemap.GetTilesBlock(tilemap.cellBounds);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (buttonOn)
+        {
+            foreach (TileBase tile in allTile)
+            {
+
+            }
+        }
+
+        /*if (Input.GetKeyDown("E") == true)
+        {
+
+        }*/
     }
 }
